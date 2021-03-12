9231f9acc03c648c1bc92a88e40bfdea36a45239
Daniel Ortiz <daniel.ortiz.n@gmail.com>
2021-03-12 02:28:46 -0600

    left arid_logreg() only to load_all()

--- R/aridanalysis.R
+++ R/aridanalysis.R
@@ -1,45 +1,3 @@
-library(testthat)
-library(tidyverse)
-library(palmerpenguins)
-library(ggplot2)
-library(GGally)
-library(grid)
-library(dplyr)
-library(glmnet)
-
-#' Function to create summary statistics and basic EDA plots. Given a data frame,
-#' this function outputs general exploratory analysis plots as well as basic
-#' statistics summarizing trends in the features of the input data.
-#'
-#'@param data_frame the input dataframe to analyze
-#'@param response the column name of the response variable
-#'@param response_type string indiating if response is 'categorical' or 'continuous'
-#'@param features a list of explanatory variable column names
-#'
-#'@returns a dataframe with a list of features and their coefficients
-#'@retusns a ggplot object containing the EDA
-#'
-#'@examples
-#'arid_eda(house_prices, 'price', 'continuous, c('rooms', 'age','garage'))
-arid_eda <- function(df, response, response_type, features=c())
-
-
-#' Function that performs a linear regression on continuous response data.
-#' This function will fit a linear regression model on the input dataframe
-#' using the response supplied and provide skLearn interface functionality
-#'
-#'@param data_frame the input dataframe to analyze
-#'@param response the column name of the response variable
-#'@param features a list of explanatory variable column names
-#'@param regularization what level of regularization to use in the model
-#'@param alpha the regularization weight parameter to use
-#'
-#'@returns a linear regression model wrapped in an sklearn style class
-#'
-#'@examples
-#'arid_linreg(df, income)
-arid_linreg <- function(df, response, features=c(), regularization=NULL, alpha = c(1))
-
 #' Given a matrix X of explanatory variables, a response y numeric variable,
 #' this function fits either a 'binomial' or 'multinomial' logistic regression model
 #' and returns a class object similar to sci-kit learn's object
@@ -168,20 +126,3 @@ arid_logreg <- function(X, y, regularization=NULL, lambda=NULL){
   class(arid_logreg) <- "arid_logreg"
   return(arid_logreg)
 }
-
-#' A function that performs linear regression on counting data when the response is
-#' restricted to be positive and natural. This function will perform count regression
-#' to the specified columns of a data frame and return a substantial inferential analysis.
-#'
-#'@param data_frame the input dataframe to analyze
-#'@param response the column name of the response variable
-#'@param features a list of explanatory variable column names
-#'@param model A string indicating the model type. Either "additive" or "interactive"
-#'@param alpha significance level for analysis
-#'
-#'@returns a wrapped statistical linear model with new functionalities.
-#'@returns a string family was used in the generalized linear regression model based on an overdispersion and fitting analysis
-#'
-#'@examples
-#'aridanalysis.arid_countreg(df, income, features = [feat1, feat5] ,"additive")
-arid_countreg <- function(data_frame, response, features=c(), model="additive", polynomial=False, alpha=0.05)
